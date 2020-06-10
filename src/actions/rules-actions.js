adef2a69d4cf6ae4c79111beea6eb58d9811eb33
Matthis <matthis.duclos@zenika.com>
2020-06-10 01:51:59 -0700

    Add thunk to communicate with a REST API

--- src/actions/rules-actions.js
+++ src/actions/rules-actions.js
@@ -1,10 +1,15 @@
-import data from "../data.json";
-
 export const RULES_LOADED = "RULES_LOADED";
 
+// This is a thunk: a function that returns a function (with dispatch as an argument)
 export function loadRules() {
-  return {
-    type: RULES_LOADED,
-    payload: data,
+  return function (dispatch) {
+    fetch("/rest/rules")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({
+          type: RULES_LOADED,
+          payload: data,
+        });
+      });
   };
 }
--- src/app-store.js
+++ src/app-store.js
@@ -1,8 +1,9 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
 import { rulesReducer } from "./reducers/rules-reducer";
 
 const appReducer = combineReducers({
   rules: rulesReducer,
 });
 
-export const store = createStore(appReducer);
+export const store = createStore(appReducer, applyMiddleware(thunkMiddleware));
